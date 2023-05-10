3f63e51f34ecf8c9cee11f32cc46be3e50577193
01Ander <arinconayala@hotmail.com>
2023-05-10 17:44:06 -0500

    dynamic routes

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,8 +1,10 @@
-export default function Post({ post }: { post: number }) {
+export default function Post({ params }: { params: { id: number } }) {
+  const { id } = params;
+
   return (
     <div>
       <h1>Post</h1>
-      <p>Esto es un post</p>
+      <p>Esto es un post {id}</p>
     </div>
   );
 }
